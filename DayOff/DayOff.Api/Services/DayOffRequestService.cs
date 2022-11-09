ea61dab44205b49f974aeec03525a2a3732a7f72
Piotr Wilk <piotr.wilk90@gmail.com>
2022-11-09 22:52:12 +0100

    Create solution and DayOffController

---
+++ DayOff/DayOff.Api/Services/DayOffRequestService.cs
@@ -0,0 +1,19 @@
+﻿using DayOff.Api.Exceptions;
+using LanguageExt;
+using LanguageExt.Common;
+
+namespace DayOff.Api.Services
+{
+    public class DayOffRequestService : IDayOffRequestService
+    {
+        public Result<Unit> RequestDayOff(DateTime from, DateTime to)
+        {
+            if (from > to)
+            {
+                return new Result<Unit>(new DateDimeRangeValidationException("End of dayOff can't be earlier than start"));
+            }
+
+            return Unit.Default;
+        }
+    }
+}
