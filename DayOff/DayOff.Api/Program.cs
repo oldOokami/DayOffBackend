698143fad3487aef92c89fedea66ee1bc03af90c
Piotr Wilk <piotr.wilk90@gmail.com>
2022-11-11 18:35:51 +0100

    Add Database

--- DayOff/DayOff.Api/Program.cs
+++ DayOff/DayOff.Api/Program.cs
@@ -1,12 +1,16 @@
 using DayOff.Api.Services;
+using DayOff.Database;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var configuration = builder.Configuration;
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddScoped<IDayOffRequestService, DayOffRequestService>();
+builder.Services.AddDbContext<DayOffDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("appDb")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
